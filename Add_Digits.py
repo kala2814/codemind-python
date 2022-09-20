3b2a8fe414a47e573b0128ebc5bbf78ab66fc9bf
kala2814 <111487860+kala2814@users.noreply.github.com>
2022-09-20 20:44:30 +0530

    n=int(input()) s=0 while n>0 or s>9:     if(n==0):         n=s         s=0     r=n%10     s+=r     n=n//10 print(s)

--- Add_Digits.py
+++ Add_Digits.py
@@ -1,3 +1,12 @@
 n=int(input())
-s=sum(int(i) for i in str(n))
-print(s%9 if s%9 else 9)
+s=0
+while n>0 or s>9:
+    if(n==0):
+        n=s
+        s=0
+    r=n%10
+    s+=r
+    n=n//10
+print(s)
+    
+   
