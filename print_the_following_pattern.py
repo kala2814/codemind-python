e4a7c7a4016d8b51c807b1869f8673e98ceed3d7
kala2814 <111487860+kala2814@users.noreply.github.com>
2022-11-03 23:02:40 +0530

    n=int(input()) for i in range(n):         for j in range(n,0,-1):             print(j,end=' ')         print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,6 +1,6 @@
 n=int(input())
-while(n>=1):
-    for i in range(1,n+1):
-        print(i,end='')
-    print()
-    n-=1
+for i in range(n):
+        for j in range(n,0,-1):
+            print(j,end=' ')
+        print()
+    
