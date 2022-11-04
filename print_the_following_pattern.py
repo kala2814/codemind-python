75b83e769d1ffebc45db25d4e893e25119931e25
kala2814 <111487860+kala2814@users.noreply.github.com>
2022-11-04 22:02:35 +0530

    hr=int(input()) for i in range(hr,0,-1):     for j in range(1,i):         print(' ',end='')     for k in range(0,hr):         if(i==1 or i==hr or k==0 or k==hr-1):             print('*',end='')         else:             print(' ',end='')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,7 +1,10 @@
-p=65
-n=int(input())
-for i in range(n):
-    for j in range(n):
-        print(chr(p),end=" ")
-    print("")
-    p+=1
+hr=int(input())
+for i in range(hr,0,-1):
+    for j in range(1,i):
+        print(' ',end='')
+    for k in range(0,hr):
+        if(i==1 or i==hr or k==0 or k==hr-1):
+            print('*',end='')
+        else:
+            print(' ',end='')
+    print()
